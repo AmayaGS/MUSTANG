998a1d06b7b6a3b149b8808ff1db579a27c2dcf2
AmayaGS <a.r.syed@qmul.ac.uk>
2023-11-08 17:43:13 +0000

    arg parser in main

--- graph_train_loop.py
+++ graph_train_loop.py
@@ -27,7 +27,7 @@ if use_gpu:
 import gc
 gc.enable()
 
-def train_graph_multi_wsi(graph_net, train_loader, test_loader, loss_fn, optimizer, n_classes, num_epochs=1, training=True, testing=True, checkpoint=True, checkpoint_path="PATH_checkpoints"):
+def train_graph_multi_wsi(graph_net, train_loader, test_loader, loss_fn, optimizer, n_classes, num_epochs=1, checkpoint=True, checkpoint_path="PATH_checkpoints"):
 
 
     since = time.time()
@@ -48,138 +48,133 @@ def train_graph_multi_wsi(graph_net, train_loader, test_loader, loss_fn, optimiz
 
         ##################################
         # TRAIN
-        if training:
+        acc_logger = Accuracy_Logger(n_classes=n_classes)
+        train_acc = 0
+        train_count = 0
+        graph_net.train()
 
-            acc_logger = Accuracy_Logger(n_classes=n_classes)
-            train_acc = 0
-            train_count = 0
-            graph_net.train()
+        print("Epoch {}/{}".format(epoch, num_epochs), flush=True)
+        print('-' * 10)
 
-            print("Epoch {}/{}".format(epoch, num_epochs), flush=True)
-            print('-' * 10)
+        for batch_idx, (patient_ID, graph_object) in enumerate(train_loader.dataset.items()):
 
-            for batch_idx, (patient_ID, graph_object) in enumerate(train_loader.dataset.items()):
+            data, label = graph_object
 
-                data, label = graph_object
+            if use_gpu:
+                data, label = data.cuda(), label.cuda()
+            else:
+                data, label = data, label
 
+            logits, Y_prob = graph_net(data)
+            Y_hat = Y_prob.argmax(dim=1)
+            acc_logger.log(Y_hat, label)
+            train_loss = loss_fn(logits, label)
+
+            train_acc += torch.sum(Y_hat == label.data)
+            train_count += 1
+
+            train_loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            del data, logits, Y_prob, Y_hat
+            gc.collect()
+
+        total_loss = train_loss.item() / train_count
+        train_accuracy =  train_acc / train_count
+
+        train_loss_list.append(total_loss)
+        train_accuracy_list.append(train_accuracy.item())
+
+        print()
+        print('Epoch: {}, train_loss: {:.4f}, train_accuracy: {:.4f}'.format(epoch, total_loss, train_accuracy))
+        for i in range(n_classes):
+            acc, correct, count = acc_logger.get_summary(i)
+            print('class {}: acc {}, correct {}/{}'.format(i, acc, correct, count), flush=True)
+
+        ################################
+        # TEST
+        graph_net.eval()
+
+        val_acc_logger = Accuracy_Logger(n_classes)
+        val_loss = 0.
+        val_acc = 0
+        val_count = 0
+
+        prob = []
+        labels = []
+
+        for batch_idx, (patient_ID, graph_object) in enumerate(test_loader.dataset.items()):
+
+            data, label = graph_object
+
+            with torch.no_grad():
                 if use_gpu:
                     data, label = data.cuda(), label.cuda()
                 else:
                     data, label = data, label
 
-                logits, Y_prob = graph_net(data)
-                Y_hat = Y_prob.argmax(dim=1)
-                acc_logger.log(Y_hat, label)
-                train_loss = loss_fn(logits, label)
+            logits, Y_prob = graph_net(data)
+            Y_hat = Y_prob.argmax(dim=1)
+            val_acc_logger.log(Y_hat, label)
 
-                train_acc += torch.sum(Y_hat == label.data)
-                train_count += 1
+            val_acc += torch.sum(Y_hat == label.data)
+            val_count += 1
 
-                train_loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
+            loss = loss_fn(logits, label)
+            val_loss += loss.item()
 
-                del data, logits, Y_prob, Y_hat
-                gc.collect()
+            prob.append(Y_prob.detach().to('cpu').numpy())
+            labels.append(label.item())
 
-            total_loss = train_loss.item() / train_count
-            train_accuracy =  train_acc / train_count
+            del data, logits, Y_prob, Y_hat
+            gc.collect()
 
-            train_loss_list.append(total_loss)
-            train_accuracy_list.append(train_accuracy.item())
+        val_loss /= val_count
+        val_accuracy = val_acc / val_count
 
-            print()
-            print('Epoch: {}, train_loss: {:.4f}, train_accuracy: {:.4f}'.format(epoch, total_loss, train_accuracy))
-            for i in range(n_classes):
-                acc, correct, count = acc_logger.get_summary(i)
-                print('class {}: acc {}, correct {}/{}'.format(i, acc, correct, count), flush=True)
+        val_loss_list.append(val_loss)
+        val_accuracy_list.append(val_accuracy.item())
 
-        ################################
-        # TEST
+        if n_classes == 2:
+            prob =  np.stack(prob, axis=1)[0]
+            val_auc = roc_auc_score(labels, prob[:, 1])
+            aucs = []
+        else:
+            aucs = []
+            binary_labels = label_binarize(labels, classes=[i for i in range(n_classes)])
+            prob =  np.stack(prob, axis=1)[0]
+            for class_idx in range(n_classes):
+                if class_idx in labels:
+                    fpr, tpr, _ = roc_curve(binary_labels[:, class_idx], prob[:, class_idx])
+                    aucs.append(calc_auc(fpr, tpr))
+                else:
+                    aucs.append(float('nan'))
 
-        if testing:
+            val_auc = np.nanmean(np.array(aucs))
 
-            graph_net.eval()
+        val_auc_list.append(val_auc)
 
-            val_acc_logger = Accuracy_Logger(n_classes)
-            val_loss = 0.
-            val_acc = 0
-            val_count = 0
+        conf_matrix = confusion_matrix(labels, np.argmax(prob, axis=1))
 
-            prob = []
-            labels = []
+        print('\nVal Set, val_loss: {:.4f}, AUC: {:.4f}, Accuracy: {:.4f}'.format(val_loss, val_auc, val_accuracy), flush=True)
 
-            for batch_idx, (patient_ID, graph_object) in enumerate(test_loader.dataset.items()):
+        print(conf_matrix)
 
-                data, label = graph_object
+        if n_classes == 2:
+            sensitivity = conf_matrix[1,1] / (conf_matrix[1,1] + conf_matrix[1,0]) # TP / (TP + FN)
+            specificity = conf_matrix[0,0] / (conf_matrix[0,0] + conf_matrix[0,1])
+            print('Sensitivity: ', sensitivity)
+            print('Specificity: ', specificity)
 
-                with torch.no_grad():
-                    if use_gpu:
-                        data, label = data.cuda(), label.cuda()
-                    else:
-                        data, label = data, label
+        if val_accuracy >= best_acc:
+            if val_auc >= best_AUC:
+                best_acc = val_accuracy
+                best_AUC = val_auc
 
-                logits, Y_prob = graph_net(data)
-                Y_hat = Y_prob.argmax(dim=1)
-                val_acc_logger.log(Y_hat, label)
-
-                val_acc += torch.sum(Y_hat == label.data)
-                val_count += 1
-
-                loss = loss_fn(logits, label)
-                val_loss += loss.item()
-
-                prob.append(Y_prob.detach().to('cpu').numpy())
-                labels.append(label.item())
-
-                del data, logits, Y_prob, Y_hat
-                gc.collect()
-
-            val_loss /= val_count
-            val_accuracy = val_acc / val_count
-
-            val_loss_list.append(val_loss)
-            val_accuracy_list.append(val_accuracy.item())
-
-            if n_classes == 2:
-                prob =  np.stack(prob, axis=1)[0]
-                val_auc = roc_auc_score(labels, prob[:, 1])
-                aucs = []
-            else:
-                aucs = []
-                binary_labels = label_binarize(labels, classes=[i for i in range(n_classes)])
-                prob =  np.stack(prob, axis=1)[0]
-                for class_idx in range(n_classes):
-                    if class_idx in labels:
-                        fpr, tpr, _ = roc_curve(binary_labels[:, class_idx], prob[:, class_idx])
-                        aucs.append(calc_auc(fpr, tpr))
-                    else:
-                        aucs.append(float('nan'))
-
-                val_auc = np.nanmean(np.array(aucs))
-
-            val_auc_list.append(val_auc)
-
-            conf_matrix = confusion_matrix(labels, np.argmax(prob, axis=1))
-
-            print('\nVal Set, val_loss: {:.4f}, AUC: {:.4f}, Accuracy: {:.4f}'.format(val_loss, val_auc, val_accuracy), flush=True)
-
-            print(conf_matrix)
-
-            if n_classes == 2:
-                sensitivity = conf_matrix[1,1] / (conf_matrix[1,1] + conf_matrix[1,0]) # TP / (TP + FN)
-                specificity = conf_matrix[0,0] / (conf_matrix[0,0] + conf_matrix[0,1])
-                print('Sensitivity: ', sensitivity)
-                print('Specificity: ', specificity)
-
-            if val_accuracy >= best_acc:
-                if val_auc >= best_AUC:
-                    best_acc = val_accuracy
-                    best_AUC = val_auc
-
-                    if checkpoint:
-                        checkpoint_weights = checkpoint_path + str(epoch) + ".pth"
-                        torch.save(graph_net.state_dict(), checkpoint_weights)
+                if checkpoint:
+                    checkpoint_weights = checkpoint_path + str(epoch) + ".pth"
+                    torch.save(graph_net.state_dict(), checkpoint_weights)
 
     elapsed_time = time.time() - since
 
--- load_train_graph_multi.py
+++ load_train_graph_multi.py
@@ -35,10 +35,25 @@ from plotting_results import auc_plot, pr_plot, confusion_matrix_plot, learning_
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Multi-stain self-attention graph multiple instance learning for Whole Slide Image set classification at the classification level")
-    # Add command line arguments
+    parser = argparse.ArgumentParser(description="Multi-stain self-attention graph multiple instance learning for Whole Slide Image set classification at the patient level")
+
+    # Command line arguments
     parser.add_argument("--dataset_name", type=str, default="RA", help="Dataset name")
     parser.add_argument("--PATH_patches", type=str, default="df_labels.csv", help="CSV file with patch file location")
+    parser.add_argument("--embedding_vector_size", type=int, default=1024, help="Embedding vector size")
+    parser.add_argument("--learning_rate", type=float, default=0.0001, help="Learning rate")
+    parser.add_argument("--pooling_ratio", type=float, default=0.7, help="Pooling ratio")
+    parser.add_argument("--heads", type=int, default=2, help="Number of GAT heads")
+    parser.add_argument("--K", type=int, default=5, help="Number of nearest neighbours in k-NNG created from WSI embeddings")
+    parser.add_argument("--train_fraction", type=float, default=0.7, help="Train fraction")
+    #parser.add_argument("--slide_batch", type=int, default=10, help="Slide batch size")
+    parser.add_argument("--num_epochs", type=int, default=1, help="Number of training epochs")
+    parser.add_argument("--n_classes", type=int, default=2, help="Number of classes")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed")
+    parser.add_argument("--num_workers", type=int, default=0, help="Number of workers for data loading")
+    parser.add_argument("--graph_batch_size", type=int, default=1, help="Graph batch size for training")
+    parser.add_argument("--checkpoint", action="store_false", default=True, help="Enable checkpointing of GNN weights. Set to False if you don't want to store checkpoints.")
+
     args = parser.parse_args()
 
     # Set environment variables
@@ -74,34 +89,31 @@ def main():
         ])
 
     # Parameters
-    state = 42
-    seed_everything(state)
-    train_fraction = .7
+    seed = args.seed
+    seed_everything(seed)
+    train_fraction = args.train_fraction
     subset= False
     slide_batch = 10 # this needs to be larger than one, otherwise Dataloader can fail when only passed a None object from collate function. TODO. change Dataset to Iterable dataset to solve this problem.
-    K=5
-    num_workers = 0
-    batch_size = 1
+    K= args.K
+    num_workers = args.num_workers
+    batch_size = args.graph_batch_size
     creating_knng = False
     creating_embedding = False
-    training = True
-    testing = True
     train_graph = True
-    embedding_vector_size = 1024
-    learning_rate = 0.0001
-    str_lr = str(learning_rate)
-    str_state = str(state)
-    pooling_ratio = 0.7
-    heads = 2
-    num_epochs = 20
-    str_pr = str(pooling_ratio)
-    str_hd = str(heads)
-    label = 'Pathotype_binary'
+    embedding_vector_size = args.embedding_vector_size
+    learning_rate = args.learning_rate
+    pooling_ratio = args.pooling_ratio
+    heads = args.heads
+    num_epochs = args.num_epochs
+
+    label = 'label'
     patient_id = 'Patient ID'
-    n_classes=2
-    checkpoint = True
+    dataset_name = args.dataset_name
+    n_classes= args.n_classes
+
+    checkpoint = args.checkpoint
     current_directory = Path(__file__).resolve().parent
-    results = os.path.join(current_directory, f"results/graph_{args.dataset_name}_{str_state}_{str_hd}_{str_pr}_{str_lr}")
+    results = os.path.join(current_directory, f"results/graph_{dataset_name}_{seed}_{heads}_{pooling_ratio}_{learning_rate}")
     os.makedirs(results, exist_ok = True)
 
    # Load the dataset
@@ -115,7 +127,7 @@ def main():
 
     # create k-NNG with VGG patch embedddings
     if creating_knng:
-        file_ids, train_ids, test_ids = Loaders().train_test_ids(df, train_fraction, state, patient_id, label, subset)
+        file_ids, train_ids, test_ids = Loaders().train_test_ids(df, train_fraction, seed, patient_id, label, subset)
         train_subset, test_subset = Loaders().df_loader(df, train_transform, test_transform, train_ids, test_ids, patient_id, label, subset=subset)
         train_slides, test_slides = Loaders().slides_dataloader(train_subset, test_subset, train_ids, test_ids, train_transform, test_transform, slide_batch=slide_batch, num_workers=num_workers, shuffle=False, collate=collate_fn_none, label=label, patient_id=patient_id)
         embedding_net = VGG_embedding(embedding_vector_size=embedding_vector_size, n_classes=n_classes)
@@ -127,28 +139,28 @@ def main():
         for file_prefix, slides in slides_dict.items():
             graph_dict, embedding_dict = create_embeddings_graphs(embedding_net, slides, k=K, mode='connectivity', include_self=False)
             print(f"Started saving {file_prefix[0]} to file")
-            with open(f"{file_prefix[0]}{args.dataset_name}.pkl", "wb") as file:
+            with open(f"{file_prefix[0]}{dataset_name}.pkl", "wb") as file:
                 pickle.dump(graph_dict, file)  # encode dict into Pickle
                 print("Done writing graph dict into pickle file")
             print(f"Started saving {file_prefix[1]} to file")
-            with open(f"{file_prefix[1]}{args.dataset_name}.pkl", "wb") as file:
+            with open(f"{file_prefix[1]}{dataset_name}.pkl", "wb") as file:
                 pickle.dump(embedding_dict, file)  # encode dict into Pickle
                 print("Done writing embedding dict into pickle file")
 
     # load pickled embeddings and graphs
     if not creating_knng:
-        with open(f"train_graph_dict_{args.dataset_name}.pkl", "rb") as train_file:
+        with open(f"train_graph_dict_{dataset_name}.pkl", "rb") as train_file:
         # Load the dictionary from the file
             train_graph_dict = pickle.load(train_file)
-        with open(f"test_graph_dict_{args.dataset_name}.pkl", "rb") as test_file:
+        with open(f"test_graph_dict_{dataset_name}.pkl", "rb") as test_file:
         # Load the dictionary from the file
             test_graph_dict = pickle.load(test_file)
 
     if not creating_embedding:
-        with open(f"train_embedding_dict_{args.dataset_name}.pkl", "rb") as train_file:
+        with open(f"train_embedding_dict_{dataset_name}.pkl", "rb") as train_file:
         # Load the dictionary from the file
             train_embedding_dict = pickle.load(train_file)
-        with open(f"test_embedding_dict_{args.dataset_name}.pkl", "rb") as test_file:
+        with open(f"test_embedding_dict_{dataset_name}.pkl", "rb") as test_file:
         # Load the dictionary from the file
             test_embedding_dict = pickle.load(test_file)
 
@@ -164,9 +176,8 @@ def main():
     sampler = torch.utils.data.sampler.WeightedRandomSampler(samples_weight.type('torch.DoubleTensor'), num_samples=len(samples_weight),  replacement=True)
 
     # MULTI-STAIN GRAPH
-    #classification_weights = PATH_output_weights + "\best" + str_hd + ".pth"
     train_graph_loader = torch.utils.data.DataLoader(train_graph_dict, batch_size=batch_size, shuffle=False, num_workers=num_workers, sampler=sampler, drop_last=False)
-    #train_graph_loader = torch_geometric.loader.DataLoader(train_graph_dict, batch_size=1, shuffle=False, num_workers=0, sampler=sampler, drop_last=False, generator=seed_everything(state))
+    #train_graph_loader = torch_geometric.loader.DataLoader(train_graph_dict, batch_size=1, shuffle=False, num_workers=0, sampler=sampler, drop_last=False, generator=seed_everything(state)) #TODO MINIBATCHING
     test_graph_loader = torch.utils.data.DataLoader(test_graph_dict, batch_size=batch_size, shuffle=False, num_workers=num_workers, drop_last=False)
 
     if train_graph:
@@ -175,11 +186,10 @@ def main():
         optimizer_ft = optim.Adam(graph_net.parameters(), lr=learning_rate)
         if use_gpu:
             graph_net.cuda()
-        #print(state, heads, pooling_ratio, learning_rate, flush=True)
 
-        graph_weights, results_dict = train_graph_multi_wsi(graph_net, train_graph_loader, test_graph_loader, loss_fn, optimizer_ft, n_classes, num_epochs=num_epochs, training=training, testing=testing, checkpoint=checkpoint, checkpoint_path= results + "/checkpoint_")
+        graph_weights, results_dict = train_graph_multi_wsi(graph_net, train_graph_loader, test_graph_loader, loss_fn, optimizer_ft, n_classes, num_epochs=num_epochs, checkpoint=checkpoint, checkpoint_path= results + "/checkpoint_")
 
-        #torch.save(graph_weights.state_dict(), classification_weights)
+        torch.save(graph_weights.state_dict(), results + ".pth")
 
         df_results = pd.DataFrame.from_dict(results_dict)
         df_results.to_csv(results + ".csv", index=False)
